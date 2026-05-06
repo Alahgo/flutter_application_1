f17a4ab415ee14e194730080c5163aae31a5c0cb
AlexUji <alejandrolahiguera8@gmail.com>
2026-05-06 18:48:15 +0200

    alahgon:  Ventana de diálogo

--- flu_avm/lib/presentation/screens/bands/bands_screen.dart
+++ flu_avm/lib/presentation/screens/bands/bands_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flu_avm/config/config.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class BandsScreen extends StatelessWidget {
   const BandsScreen({super.key});
@@ -17,7 +19,11 @@ class BandsScreen extends StatelessWidget {
         itemBuilder: (context, index) {
           return _bandTile(bands[index]);
         },
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 1,
+        onPressed: () => addereNuvumBand(context),
+        child: Icon(Icons.add),),
     );
   }
 
@@ -33,4 +39,63 @@ class BandsScreen extends StatelessWidget {
           },
         );
   }
+
+  addereNuvumBand(BuildContext context){
+
+    final textumController = TextEditingController();
+
+    
+    /*showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("New band name:"),
+          content: TextField(controller: textumController,),
+          actions: [
+            MaterialButton(
+              onPressed:() => addereBandAdCollectione(context, textumController.text),
+              textColor: Colors.blue,
+              child: Text("add"), 
+            )
+          ],
+        );
+      },
+    );*/
+    showCupertinoDialog(
+    context: context,
+    builder: (BuildContext context) => CupertinoAlertDialog(
+      title: const Text('New band name'),
+      content: CupertinoTextField(
+        controller: textumController,
+        style: TextStyle(
+          color: Theme.of(context).brightness == Brightness.dark
+              ? Colors.white
+              : Colors.black,
+      ),
+    ),
+    
+    actions: [
+      CupertinoDialogAction(
+        isDefaultAction: true,
+        child: const Text('Add'),
+        onPressed: () {
+          addereBandAdCollectione(context, textumController.text);
+          context.pop();
+        },
+      ),
+      CupertinoDialogAction(
+        isDestructiveAction: true,
+        child: const Text('Close'),
+        onPressed: () => context.pop(),
+      ),
+    ],
+  ),
+);
+  }
+
+  void addereBandAdCollectione(BuildContext context, String nomen){
+    print(nomen);
+    context.pop();
+  }
+
 }
