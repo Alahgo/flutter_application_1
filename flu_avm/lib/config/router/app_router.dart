11f96344d862172872ba00105ea54ce571b81515
Alahgon <alejandrolahiguera8@gmail.com>
2026-05-18 10:17:10 +0200

    alahgon: Formulario del mapa

--- flu_avm/lib/config/router/app_router.dart
+++ flu_avm/lib/config/router/app_router.dart
@@ -13,6 +13,9 @@ final appRouter = GoRouter(
       ),
       GoRoute(path: '/bands',
       builder: (context, state)=> const BandsScreen()
+      ),
+      GoRoute(path: '/charta',
+      builder: (context, state)=> const ChartaScreen()
       )
   ]
 );
