d318a4f1069e348c371983f7e5aaa7528bef7490
Alahgon <alejandrolahiguera8@gmail.com>
2026-05-18 10:33:50 +0200

    alahgon: Color y Nombre en StateProvider

---
+++ flu_avm/lib/presentation/providers/charta_provider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/legacy.dart';
+
+
+final formColorProvider = StateProvider<Color>((ref) => Colors.red);
+final formNomenProvider = StateProvider<String>((ref) => '');
